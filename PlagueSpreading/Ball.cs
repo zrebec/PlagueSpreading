b4461f17251c386857f06f27ef9bd704a0657125
Martin Fox <zrebec@zrebec.sk>
2020-03-29 15:49:36 +0200

    Add project files.

---
+++ PlagueSpreading/Ball.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Drawing;
+using System.Security.Cryptography;
+
+namespace PlagueSpreading
+{
+    class Ball
+    {
+        private const int ballWidth = 15;
+        private const int ballHeight = 15;
+        public double radius = (double)(ballWidth / 2) + (double)(ballHeight / 2);
+        private int canvasWidth;
+        private int canvasHeight;
+        public int ballX, ballY; //Position
+        public int ballVx, ballVy; //Velocity
+        public bool affected = false;
+        private RNGCryptoServiceProvider Rand = new RNGCryptoServiceProvider();
+
+        public void init(int canvasWidth, int canvasHeight)
+        {
+            ballVx = RandomInteger(1, 6);
+            ballVy = RandomInteger(1, 6);
+            this.canvasWidth = canvasWidth;
+            this.canvasHeight = canvasHeight;
+            ballX = RandomInteger(0, this.canvasWidth - ballWidth);
+            ballY = RandomInteger(0, this.canvasHeight - ballHeight);
+            Console.WriteLine("X, Y, vX, vY: " + ballX + ", " + ballY + ", " + ballVx + ", " + ballVy);
+        }
+
+        public void update()
+        {
+            ballX += ballVx;
+            if (ballX < 0)
+            {
+                ballVx = -ballVx;   
+            }
+            else if (ballX + ballWidth > canvasWidth)
+            {
+                ballVx = -ballVx;
+            }
+
+            ballY += ballVy;
+            if (ballY < 0)
+            {
+                ballVy = -ballVy;
+            }
+            else if (ballY + ballHeight > canvasHeight)
+            {
+                ballVy = -ballVy;
+            }
+        }
+
+        public void updateBall(Ball b1, Ball b2)
+        {
+            if (b1.ballX < b2.ballX)
+            {
+                ballVx = -ballVx;
+            }
+            else if (ballX + ballWidth > canvasWidth)
+            {
+                ballVx = -ballVx;
+            }
+
+            ballY += ballVy;
+            if (ballY < 0)
+            {
+                ballVy = -ballVy;
+            }
+            else if (ballY + ballHeight > canvasHeight)
+            {
+                ballVy = -ballVy;
+            }
+        }
+
+        public void paint(System.Windows.Forms.PaintEventArgs e)
+        {
+            if(affected)
+                e.Graphics.FillEllipse(Brushes.Red, ballX, ballY, ballWidth, ballHeight);
+            else
+                e.Graphics.FillEllipse(Brushes.Blue, ballX, ballY, ballWidth, ballHeight);
+        }
+
+        // Return a random integer between a min and max value.
+        private int RandomInteger(int min, int max)
+        {
+            uint scale = uint.MaxValue;
+            while (scale == uint.MaxValue)
+            {
+                // Get four random bytes.
+                byte[] four_bytes = new byte[4];
+                Rand.GetBytes(four_bytes);
+
+                // Convert that into an uint.
+                scale = BitConverter.ToUInt32(four_bytes, 0);
+            }
+
+            // Add min to the scaled difference between max and min.
+            return (int)(min + (max - min) *
+                (scale / (double)uint.MaxValue));
+        }
+
+    }
+}
---
+++ PlagueSpreading/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PlagueSpreading
+{
+    public partial class Form1 : Form
+    {
+        readonly Ball[] b = new Ball[20];
+        private const int FPS = 30;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelStatus.Text = "I'm begining";
+            // Pick a random start position and velocity
+            for (int i = 0; i < b.Length; i++)
+            {
+                b[i] = new Ball();
+                b[i].init(ClientSize.Width, ClientSize.Height);
+                labelStatus.Text = "Initialization of ball " + i + " is done";
+            }
+
+            this.SetStyle(
+             ControlStyles.AllPaintingInWmPaint |
+             ControlStyles.UserPaint |
+             ControlStyles.OptimizedDoubleBuffer,
+             true);
+            this.UpdateStyles();
+            timer1.Interval = (int) Math.Ceiling( (double) 1000 / FPS);
+        }
+
+        private double distance(Ball b1, Ball b2)
+        {
+            double x = (double) b1.ballX - b2.ballX;
+            double y = (double)b1.ballY - b2.ballY;
+            //Console.WriteLine("x, y: " + x + ", " + y);
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < b.Length; i++)
+            {
+                for (int j = 0; j < b.Length; j++)
+                {
+                    if (j != i)
+                    {
+                        if (distance(b[i], b[j]) <= b[i].radius)
+                        {
+                            //TODO Think about path collision update. It is right to change vector only for i ball?
+                            //In fact, both balls change the directrion
+                            b[i].ballVx = -b[i].ballVx;
+                            //b[j].ballVy = -b[j].ballVy;
+                            b[j].ballVy = -b[j].ballVy;
+
+                            // Set affected status
+                            if (b[i].affected == true && b[j].affected == false)
+                            {
+                                labelStatus.Text = "Patient " + i + " infected person " + j;
+                                b[j].affected = true;
+                            }
+                            else if (b[j].affected == true && b[i].affected == false)
+                            {
+                                labelStatus.Text = "Patient " + j + " infected person " + i;
+                                b[i].affected = true;
+                            }
+                                
+                        }
+                    }
+                }
+                b[i].update();
+            }
+
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            e.Graphics.Clear(BackColor);
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (i == 0) b[i].affected = true;
+                b[i].paint(e);
+                
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            for (int i = 0; i < b.Length; i++)
+            {
+                b[i].init(ClientSize.Width, ClientSize.Height);
+            }
+        }
+    }
+}
